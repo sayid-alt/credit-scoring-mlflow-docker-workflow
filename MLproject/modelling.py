27b7ba06d5e681f2c846dff6e508ecc66572a0c5
Heykal Sayid <heykalsayid@gmail.com>
2026-06-06 00:28:16 +0700

    Nested true in mlflow start

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -7,6 +7,8 @@ import numpy as np
 import warnings
 import sys
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
@@ -17,15 +19,15 @@ if __name__ == "__main__":
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     X_train, X_test, y_train, y_test = train_test_split(
-    data.drop("Credit_Score", axis=1),
-    data["Credit_Score"],
-    random_state=42,
-    test_size=0.2
+        data.drop("Credit_Score", axis=1),
+        data["Credit_Score"],
+        random_state=42,
+        test_size=0.2
     )
     input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
-    with mlflow.start_run():
+    with mlflow.start_run(nested=True):
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
 
